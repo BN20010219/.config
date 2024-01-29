e33ff2750867f16acba2985af8c1a3cc634c1472
narzary <bar.narzary1@gmail.com>
2024-01-29 23:09:10 +0530

    Changed the deafult alpha dashboard

--- nvim/lua/narzary/plugins/alpha-nvim.lua
+++ nvim/lua/narzary/plugins/alpha-nvim.lua
@@ -2,6 +2,18 @@ return{
   "goolord/alpha-nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require"alpha".setup(require"alpha.themes.dashboard".config)
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    dashboard.section.buttons.val = {
+      dashboard.button("e","  New file" , ":ene <BAR> startinsert <CR>" ),
+      dashboard.button("l", "Lazy", ":Lazy<CR>"),
+      dashboard.button("t", "Nvim-tree", ": NvimTreeOpen<CR>"),
+      dashboard.button( "q", "󰅚  Quit NVIM" , ":qa<CR>" )
+    }
+
+    dashboard.config.opts.noautocmd = true
+    vim.cmd[[autocmd User AlphaReady echo 'READY FOR A NEW SESSION']]
+    alpha.setup(dashboard.config)
   end
 }
